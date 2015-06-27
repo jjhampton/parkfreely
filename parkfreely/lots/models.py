1f561b85f835cbe6535225a5fee28e2da2e778fc
Lance Rogers <lancekincaid1994@gmail.com>
2015-06-27 14:07:30 -0400

    eh

--- parkfreely/lots/models.py
+++ parkfreely/lots/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from geolocation.google_maps import GoogleMaps
 # Create your models here.
 
-class Lot(models.Model, longit, latit, rating):
+class Lot(models.Model):
      lon = models.FloatField()
      lat = models.FloatField()
      def __init__(self, longit, latit, rating):
