202f83fa856d3e1e28fb6b674ee2f9f3b9b14a48
Lance Rogers <lancekincaid1994@gmail.com>
2015-06-27 12:55:15 -0400

    .gitignore updated

--- parkfreely/lots/models.py
+++ parkfreely/lots/models.py
@@ -3,5 +3,7 @@ from geolocation.google_maps import GoogleMaps
 # Create your models here.
 
 class Lot(models.Model):
+     __init__(self):
+
      lon = models.FloatField()
      lat = models.FloatField()
