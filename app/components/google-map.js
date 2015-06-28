c72aeac4f57dbbeba19c081d48331a0325d886c9
John Hampton <john_j_hampton@yahoo.com>
2015-06-28 00:57:56 -0400

    Add marker object to center of Google maps, displayed on both add and search route currently

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -1,17 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    insertMap: function() {
-        var options = {
-            center: new window.google.maps.LatLng(
-                this.get('latitude'),
-                this.get('longitude')
-            ),
-            zoom: 15
-        };
-        new window.google.maps.Map(this.$('.map-canvas')[0], options);
-        $(window).resize(function() {
-          google.maps.event.trigger(map, "resize");
-        });
-    }.on('didInsertElement')
+  insertMap: function() {
+    var myLatlng = new google.maps.LatLng(this.get('latitude'),
+    this.get('longitude'));
+    var options = {
+        center: new window.google.maps.LatLng(this.get('latitude'),
+        this.get('longitude')),
+        zoom: 15
+    };
+
+    var map = new window.google.maps.Map(this.$('.map-canvas')[0], options);
+
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+    });
+  }.on('didInsertElement')
 });
