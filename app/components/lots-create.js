1b82dd2d7ab2020c15e3504bd99b3ce355e1b56d
John Hampton <john_j_hampton@yahoo.com>
2015-07-30 19:40:13 -0400

    Insert Handlebars helpers in lots-create component in place of normal HTML inputs, awaiting connection to Parse w/ ember-parse-adapter

--- app/components/lots-create.js
+++ app/components/lots-create.js
@@ -1,4 +1,58 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'form',
+  classNames: ['add-form'],
+  safetyNumbers: [1,2,3,4,5,6,7,8,9,10],
+  currentSafetyNumber: 1,
+  actions: {
+    submit: function() {
+     var latitude = $('.user-latitude').text();
+     var longitude = $('.user-longitude').text();
+     var description = $('.add-form-description').val();
+     var biking;
+     var walking;
+     var publictransit;
+     var safety = $('.safety :selected').text();
+
+      if ($(".add-form-biking").prop('checked')) {
+        biking = true;
+      }
+      else {
+        biking = false;
+      }
+      if ($(".add-form-walking").prop('checked')) {
+        walking = true;
+      }
+      else {
+        walking = false;
+      }
+      if ($(".add-form-publictransit").prop('checked')) {
+        publictransit = true;
+      }
+      else {
+        publictransit = false;
+      }
+      console.log(latitude);
+      console.log(longitude);
+      console.log(description);
+      console.log(biking);
+      console.log(walking);
+      console.log(publictransit);
+      console.log(safety);
+      $.ajax({
+        method: "POST",
+        url: "localhost:8000/lots/post",
+        data: {
+          "latitude": latitude,
+          "location": longitude,
+          "description": description,
+          "biking": biking,
+          "walking": walking,
+          "publictransit": publictransit,
+          "safety": safety
+          }
+       });
+    }.bind(this)
+  }
 });
--- app/models/lot.js
+++ app/models/lot.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  latitude: DS.attr('number'),
+  longitude: DS.attr('number'),
+  description: DS.attr('string'),
+  biking: DS.attr('string'),
+  walking: DS.attr('string'),
+  publictransit: DS.attr('string'),
+  safety: DS.attr('number')
 });
