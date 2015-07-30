38f7692781bc7c2eeb8483bfa2c8401dd8014158
John Hampton <john_j_hampton@yahoo.com>
2015-07-30 18:34:18 -0400

    Adjust leaflet map options to set zoom and pan to current user location at marker on map load

--- app/components/leaflet-map.js
+++ app/components/leaflet-map.js
@@ -3,18 +3,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   didInsertElement: function() {
 
-    // marker that will be centered on map
-    var marker = null;
 
     //map tile
     var mapBoxBackground = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     id: 'jjhampton.f68b0cad',
     accessToken: 'pk.eyJ1IjoiampoYW1wdG9uIiwiYSI6ImEwM2Y0NWRjYTMxYTYzNGZlYzgxOGNmMzBhZmE3MDUzIn0.SN40uu8Q9bCtOf6stNTjZA',
+    minZoom: 0,
+    maxZoom: 20
     });
 
     // map object, set to locate geolocation
-    var map = L.map('lots-create-map',{attributionControl: false}).locate({setView: true, maxZoom: 16});
+    var map = L.map('lots-create-map',{attributionControl: false}).locate({setView: true});
 
     // add tile to map
     mapBoxBackground.addTo(map);
@@ -22,8 +22,10 @@ export default Ember.Component.extend({
     // add marker to map at user's location
 
     function onLocationFound(e) {
-      L.marker(e.latlng).addTo(map)
+      var marker = L.marker(e.latlng).addTo(map)
         .bindPopup("You are at this location").openPopup();
+        map.setZoom(16);
+        map.panTo(e.latlng);
     }
 
     map.on('locationfound', onLocationFound);
