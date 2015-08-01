bd2df8fed2012b1edb02611fda743cd7fd18d1eb
John Hampton <john_j_hampton@yahoo.com>
2015-08-01 16:31:55 -0400

    Restyle and layout lots-index page Generate lots-show component with computed properties for biking, walking, publictransit

--- app/models/lot.js
+++ app/models/lot.js
@@ -7,5 +7,30 @@ export default DS.Model.extend({
   biking: DS.attr('boolean'),
   walking: DS.attr('boolean'),
   publictransit: DS.attr('boolean'),
-  safety: DS.attr('number')
+  safety: DS.attr('number'),
+  bikingShow: Ember.computed('biking', function() {
+    if (this.get('biking')) {
+      return "Yes";
+    }
+    else {
+      return "No";
+    }
+  }),
+  walkingShow: Ember.computed('walking', function() {
+    if (this.get('walking')) {
+      return "Yes";
+    }
+    else {
+      return "No";
+    }
+  }),
+  publicTransitShow: Ember.computed('publictransit', function() {
+    if (this.get('publictransit')) {
+      return "Yes";
+    }
+    else {
+      return "No";
+    }
+  })
+
 });
